834cf8c6b7bf2240a64fa1188c9bc4aa01bbdb5a
Valerii Hiora <valerii.hiora@gmail.com>
2013-07-11 18:28:14 +0300

    Working message retrieving on Facebook
    Has basic date-based filtering for threads and messages

--- src/almanac/social_adapter/facebook.clj
+++ src/almanac/social_adapter/facebook.clj
@@ -1,7 +1,8 @@
 (ns almanac.social-adapter.facebook
   (:require [almanac.storage :as ss]
             [almanac.social-adapter :refer [update-activity]]
-            [almanac.social-api.facebook :as fb]))
+            [almanac.social-api.facebook :as fb])
+  (:import [java.util Date]))
 
 (defn- fb-message->ActivityItem [msg]
   nil)
@@ -12,9 +13,24 @@
 (defn- fb-photo->ActivityItem [photo]
   nil)
 
+;; TODO: move out those functions somewhere to the utils?
+(defn- date-diff-in-seconds [ref-date date]
+  "Difference (in seconds) between ref-date and date. Negative means date is earlier than ref-date"
+  (- (.getTime ref-date) (.getTime date)))
+
+(defn- gen-date-diff-predicate [ref-date sec-interval]
+  "Constructs a predicate which is true for all dates close than sec-interval to ref-date"
+  (fn [date]
+    (< (date-diff-in-seconds ref-date date) sec-interval)))
+
+(def ^:private MSECS-IN-MONTH (* 1000 60 60 24 31))
+
+(def ^:private recent-enough? (gen-date-diff-predicate (Date.) MSECS-IN-MONTH))
+;; end of section to be moved
+
 (defmethod update-activity :facebook [network user-id storage]
   (let [creds (ss/get-credentials storage :facebook user-id)]
-    (doseq [[get-fn convert-fn] [[fb/get-messages fb-message->ActivityItem]
+    (doseq [[get-fn convert-fn] [[#(fb/get-messages % :filters {:thread-filter-fn recent-enough?}) fb-message->ActivityItem]
                                  [fb/get-mentions fb-status->ActivityItem]
                                  [fb/get-photo-mentions fb-photo->ActivityItem]]]
       (->> (get-fn creds)
--- src/almanac/social_api/facebook.clj
+++ src/almanac/social_api/facebook.clj
@@ -1,20 +1,88 @@
 (ns almanac.social-api.facebook
-  (:require [clj-facebook-graph.auth :refer [with-facebook-auth]]
-            [clj-facebook-graph.client :as client]))
+  (:require [clojure.core.reducers :as r]
+            [clojure.set :as cs]
+            [clj-facebook-graph.auth :refer [with-facebook-auth]]
+            [clj-facebook-graph.client :as client])
+  (:import [java.util Date]
+           [java.text SimpleDateFormat]))
 
 ;; TODO: find out what's wrong with built-in with-facebook-auth as it doesn't send
 ;; access token to the FB server
 ;; meanwhile a very dirty workaround with a token transferred through params
-(defn- api-call [credentials path]
+(defn api-call [credentials path & {:keys [params]}]
   (with-facebook-auth credentials
-    (client/get path {:query-params {:access_token (:access-token credentials)}
-                      :extract :data})))
+    (client/get path {:query-params (merge {:access_token (:access-token credentials)}
+                                           params)})))
 
 (defn get-mentions [credentials]
-  (api-call credentials [:me :tagged]))
+  (:data (:body (api-call credentials [:me :tagged]))))
 
-(defn get-messages [credentials]
-  (api-call credentials [:me :inbox]))
+(def fb-time-format (SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ssZ"))
+
+(defn- convert-fb-timestamp [text]
+  (.parse fb-time-format text))
+
+(defn- thread-update-time [{:keys [updated_time]}]
+  (convert-fb-timestamp updated_time))
+
+
+;; Facebook message is a map with:
+;;    id = message id
+;;    thread-id = id of parent thread
+;;    text = text of message
+;;    from = user-id of sender
+;;    to = set of all recepients
+;;    created-time = time of creation (available only for comments), heuristics for start msg
+
+;; Thread has 2 different things - the starting message in the root of response
+;; and all other marked as comments/data
+
+(defn- time-for-start-msg [thread comments]
+  (if (empty? comments)
+    (convert-fb-timestamp (:updated_time thread))
+    (:created-time (first comments))))
+
+(defn- start-msg->fb-msg [user-id thread comments recipients]
+  (let [from (or (get-in thread [:from :id])
+                 user-id)]
+    {:text (:message thread)
+     :id (:id thread)
+     :thread-id (:id thread)
+     :from from
+     :to (cs/difference recipients (set (vector from)))
+     :created-time (time-for-start-msg thread comments)}))
+
+(defn- thread-comment->fb-msg [thread comment recipients]
+  (let [from (or (get-in thread [:from :id]))]
+    {:text (:message comment)
+     :thread-id (:id thread)
+     :created-time (convert-fb-timestamp (:created_time comment))
+     :from from
+     :id (:id comment)
+     :to (cs/difference recipients (set (vector from)))}))
+
+(defn get-thread-messages [credentials thread-id filters]
+  (let [thread (:body (api-call credentials [(format "%s" thread-id)]))
+        recipients (set (map :id (:data (:to thread))))
+        time-filter (or (:message-filter-fn filters)
+                        (constantly true))
+        comments (->> (get-in thread [:comments :data])
+                      (r/map #(thread-comment->fb-msg thread % recipients))
+                      (r/filter #(time-filter (:created-time %)))
+                      (into []))
+        start-msg (start-msg->fb-msg "user-id-goes-here" thread comments recipients)]
+    (cons start-msg comments)))
+
+(defn get-messages [credentials & {:keys [filters]}]
+  "Returns a sequence of private messages to/from user"
+  (let [time-filter (or (:thread-filter-fn filters)
+                        (constantly true))]
+    (->> (api-call credentials [:me :inbox] :params {:fields "updated_time"})
+         :body
+         :data
+         (r/filter #(time-filter (thread-update-time %)))
+         (r/map #(get-thread-messages credentials (:id %) filters))
+         (reduce concat))))
 
 (defn get-photo-mentions [credentials]
-  (api-call credentials [:me :photos]))
+  (:data (:body (api-call credentials [:me :photos]))))
