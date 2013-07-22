f68ee828df50a57d0e0e377ddb2285c69b1bdb2c
Valerii Hiora <valerii.hiora@gmail.com>
2013-07-22 21:53:29 +0300

    Easier system start-stop in REPL

--- dev/user.clj
+++ dev/user.clj
@@ -1,5 +1,14 @@
 (ns user
   (:require [clojure.tools.namespace.repl :refer [refresh]]
             [almanac.core :as core]
+            [almanac.system :refer [dev-system prod-system]]
             [clojure.repl :refer [doc]]
             [clojure.pprint :refer [pprint]]))
+
+(def ^:dynamic sys nil)
+
+(defn start []
+  (alter-var-root (var sys) (constantly (dev-system))))
+
+(defn stop []
+  (alter-var-root (var sys) (constantly nil)))
